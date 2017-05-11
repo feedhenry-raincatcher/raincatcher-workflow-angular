0d4ad8a5727a4f7543b7700fe0c2e6cd005a2cd9
spriadka <spriadka@github.com>
2017-05-11 11:45:24 +0200

    workflow main page default text

--- dist/workflow-empty.tpl.html.js
+++ dist/workflow-empty.tpl.html.js
@@ -9,7 +9,7 @@ ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/workflow-empty.tpl.html',
     '<div class="empty-state" layout-padding layout-margin>\n' +
     '  <h2 class="md-title">No workflow selected.</h2>\n' +
-    '  <p class="md-body-1">Select a workflow from the menu.</p>\n' +
+    '  <p class="md-body-1">Select a workflow from the menu, or create a new workflow:</p>\n' +
     '  <md-button class="md-raised md-accent" ui-sref="app.workflow.add">New Workflow</md-button>\n' +
     '</div>\n' +
     '');
